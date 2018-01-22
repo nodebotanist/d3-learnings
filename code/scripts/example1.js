75865982e3aa0109f2c86ff115380aaba7902bd5
Mx. Kas Perch <the@nodebotani.st>
2018-01-22 12:56:52 -0600

    Chapter 5 read

--- code/scripts/example1.js
+++ code/scripts/example1.js
@@ -1,5 +1,19 @@
+let rowConverter = (data) => {
+  return {
+    Food: data.Food,
+    Deliciousness: parseFloat(data.Deliciousness)
+  }
+}
+
 d3.csv('../data/food.csv', (data) => {
   console.log(data)
+
+  d3.select('body')
+    .data(data)
+    .enter()
+    .append('p')
+    .text((data) => {
+      return (data.Food + ': ' + data.Deliciousness)
+    })
 })
 
-d3.select('#visualization').append('p').text('D3 added this paragraph!')
