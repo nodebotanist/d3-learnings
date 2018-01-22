616582ed49be56c71733bedebc9afcb8491e521a
Mx. Kas Perch <the@nodebotani.st>
2018-01-22 13:01:33 -0600

    Chapter 5 example complete

--- code/scripts/example1.js
+++ code/scripts/example1.js
@@ -15,5 +15,16 @@ d3.csv('../data/food.csv', (data) => {
     .text((data) => {
       return (data.Food + ': ' + data.Deliciousness)
     })
+    .style('color', (data) => {
+      if (data.Deliciousness > 9) {
+        return '#00F'
+      } else if (data.Deliciousness > 6) {
+        return '#0F0'
+      } else if (data.Deliciousness > 2) {
+        return '#F00'
+      } else {
+        return '#AAA'
+      }
+    })
 })
 
